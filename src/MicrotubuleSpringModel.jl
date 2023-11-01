6c165a594f16866dc45c63cfe78835b83a61a953
Jeremy Worsfold <jeremyworsfold2@gmail.com>
2023-11-01 09:40:26 +0000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/MicrotubuleSpringModel.jl
@@ -0,0 +1,5 @@
+module MicrotubuleSpringModel
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MicrotubuleSpringModel
+using Test
+
+@testset "MicrotubuleSpringModel.jl" begin
+    # Write your tests here.
+end
